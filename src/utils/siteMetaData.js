211013ba473bc52e2c083ecdde16ab7d6d58fee0
mohsalim <moharts2000@gmail.com>
2023-12-20 17:50:31 +0300

    all

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,12 +1,13 @@
 // please update required information
 const siteMetadata = {
   title: "Next.js Blog With Tailwind CSS and Contentlayer",
-  author: "CodeBucks",
-  headerTitle: "Next.js Blog",
-  description: "A blog created with Next.js, Tailwind.css and contentlayer.",
+  author: "Salfinity",
+  headerTitle: "Dr. Mohamed Bahaidar",
+  description:
+    " CEO IQRAFM | AUTHOR & SCHOLAR | BUSINESS CONSULTANT | MOTIVATIONAL SPEAKER.",
   language: "en-us",
   theme: "system", // system, dark or light
-  siteUrl: "http://localhost:3000", // your website URL
+  siteUrl: "https://dr-bahaidar-blog.vercel.app", // your website URL
   siteLogo: "/logo.png",
   socialBanner: "/social-banner.png", // add social banner in the public folder
   email: "codebucks27@gmail.com",
