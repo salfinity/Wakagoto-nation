d1374d94b47e9dea7fea92025604978ebdf6011a
mohsalim <moharts2000@gmail.com>
2023-12-24 02:19:13 +0300

    update

--- src/app/(about)/about/page.js
+++ src/app/(about)/about/page.js
@@ -1,6 +1,7 @@
 import AboutCoverSection from "/src/components/About/AboutCoverSection";
 import Skills from "/src/components/About/Skills";
 import Link from "next/link";
+import Projects from "/src/components/About/Projects";
 
 export const metadata = {
   title: "About Me",
@@ -11,6 +12,7 @@ export default function About() {
   return (
     <>
       <AboutCoverSection />
+      <Projects />
       <Skills />
 
       <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
---
+++ src/components/About/projects.js
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+import { projectsData } from "/lib/data";
+import Project from "./project";
+
+export default function Projects() {
+  return (
+    <section
+      id="projects"
+      className="scroll-mt-28 max-w-6xl flex flex-col mb-64  mx-auto justify-center items-center cursor-pointer"
+    >
+      <h2 className="mb-4 uppercase text-3xl tracking-[14px] text-black dark:text-white">
+        My Profile
+      </h2>
+      <span className="font-semibold text-lg sm:text-3xl md:text-4xl text-accent dark:text-accentDark">
+        Have a look at it...
+      </span>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
