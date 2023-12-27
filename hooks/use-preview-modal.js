60dd69f1bf81e06a0bf61f504d713441cf2bf805
mohsalim <moharts2000@gmail.com>
2023-12-27 17:59:10 +0300

    all

--- hooks/use-preview-modal.js
+++ hooks/use-preview-modal.js
@@ -1,13 +1,10 @@
 import { create } from "zustand";
 
-const usePreviewModal =
-  create <
-  PreviewModalStore >
-  ((set) => ({
-    isOpen: false,
-    data: undefined,
-    onOpen: (data) => set({ isOpen: true, data }),
-    onClose: () => set({ isOpen: false }),
-  }));
+const usePreviewModal = create((set) => ({
+  isOpen: false,
+  data: undefined,
+  onOpen: (data) => set({ isOpen: true, data }),
+  onClose: () => set({ isOpen: false }),
+}));
 
 export default usePreviewModal;
