9c1860d5edb1462458a4f1f900df63d111b776be
mohsalim <moharts2000@gmail.com>
2023-12-28 04:16:54 +0300

    all

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -5,7 +5,7 @@ const TransitionEffect = () => {
   return (
     <>
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-pink-400"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#e6ac00]"
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: "0%", width: "0%" }}
         transition={{ duration: 0.8, ease: "easeInOut" }}
@@ -17,7 +17,7 @@ const TransitionEffect = () => {
         transition={{ delay: 0.2, duration: 0.8, ease: "easeInOut" }}
       />
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-neutral-900"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#1f505a]"
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: "0%", width: "0%" }}
         transition={{ delay: 0.4, duration: 0.8, ease: "easeInOut" }}
