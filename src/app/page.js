15024a4dff4ef4376b1fa223f017f9d2a1e07b13
mohsalim <moharts2000@gmail.com>
2024-01-05 02:16:02 +0300

    insights

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,27 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import CircleQuotes from "../components/Blog/CircleQuotes";
 import RadioWindow from "../components/radio/RadioWindow";
+import InsightRoll from "/src/components/About/InsightRoll";
+
+const insights = [
+  "Holy Quran recitations",
+  "Quran Translations in English, Swahili, Somali, and Urdu",
+  "Talks by Prominent scholars from Kenya and abroad",
+  "Question and Answer sessions on religious matters",
+  "Panel discussions by prominent Muslim Ulaamas on Hadith and Fiqh",
+  "Enlightening program on Teaching yourself Arabic",
+  "Educative program in Kikuyu Language",
+  "Local and international news at frequent intervals daily",
+  "Talk Shows on Contemporary issues, political, cultural, social and economic issues",
+  "Health and educational programmes",
+  "Outside broadcasting services",
+  "Studio production services for recording programs or commercials/advertisements",
+];
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center">
+      <InsightRoll insights={insights} />
       <RadioWindow />
       <HomeCoverSection blogs={allBlogs} />
       <FeaturedPosts blogs={allBlogs} />
